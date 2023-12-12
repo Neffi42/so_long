aad439ab7ae7f54a7011f7a0a4aabeaa48676e34
Andrei Basdereff <abasdere@paul-f4Ar6s5.clusters.42paris.fr>
2023-12-12 12:39:11 +0100

    one line

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 11:30:55 by abasdere          #+#    #+#             */
-/*   Updated: 2023/12/12 12:28:54 by abasdere         ###   ########.fr       */
+/*   Updated: 2023/12/12 12:38:45 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,20 @@ static int	handle_no_event(void *mlx)
 	return (0);
 }
 
+static void	fill_window(t_data *data)
+{
+	size_t	i;
+
+	i = 0;
+	data->imgs[0].img = mlx_xpm_file_to_image(data->mlx, \
+	"./textures/floor.xpm", &data->imgs[0].width, &data->imgs[0].height);
+	while (i < WIN_WIDTH)
+	{
+		mlx_put_image_to_window(data->mlx, data->win, data->imgs[0].img, i, 0);
+		i += 52;
+	}
+}
+
 int	main(int ac, const char **av)
 {
 	t_data	data;
@@ -35,9 +49,7 @@ int	main(int ac, const char **av)
 	init_data(&data);
 	check_maps(ac, av, &data);
 	init_mlx(&data);
-	data.imgs[0].img = mlx_xpm_file_to_image(data.mlx, \
-	"./textures/floor.xpm", &data.imgs[0].width, &data.imgs[0].height);
-	mlx_put_image_to_window(data.mlx, data.win, data.imgs[0].img, 0, 0);
+	fill_window(&data);
 	mlx_loop_hook(data.mlx, &handle_no_event, &data);
 	mlx_hook(data.win, KeyPress, KeyPressMask, &event_keypress, &data);
 	mlx_hook(data.win, DestroyNotify, 0, &event_destroy, &data);
